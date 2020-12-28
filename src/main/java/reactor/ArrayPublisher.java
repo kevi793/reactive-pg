2a994a58fd8f486eb3bdb4ff4a53a4d5ce3dcfe1
Vivek Pandey <viv.793@gmail.com>
2020-12-28 14:41:59 +0800

    Added tests given by TCK and making sure the publisher works fine for those test cases

--- src/main/java/reactor/ArrayPublisher.java
+++ src/main/java/reactor/ArrayPublisher.java
@@ -4,6 +4,8 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class ArrayPublisher<T> implements Publisher<T> {
 
     private final T[] array;
@@ -16,37 +18,63 @@ public class ArrayPublisher<T> implements Publisher<T> {
     public void subscribe(Subscriber<? super T> subscriber) {
         subscriber.onSubscribe(new Subscription() {
 
+            final AtomicLong requested = new AtomicLong(0);
             private int index = 0;
             private boolean completed = false;
+            private boolean cancelled = false;
+            private boolean inFlight = false;
 
             @Override
             public void request(long l) {
-                if (!completed) {
-                    while (index < ArrayPublisher.this.array.length && l > 0) {
 
-                        T item = ArrayPublisher.this.array[index];
-
-                        if (item == null) {
-                            completed = true;
-                            subscriber.onError(new NullPointerException());
-                            return;
-                        }
-
-                        index++;
-                        l--;
-                        subscriber.onNext(item);
-                    }
-
-                    if (index == ArrayPublisher.this.array.length) {
-                        subscriber.onComplete();
-                        completed = true;
-                    }
+                if (completed || cancelled) {
+                    return;
                 }
+
+                if (l <= 0) {
+                    cancel();
+                    subscriber.onError(new IllegalArgumentException("Positive request amount required but it was " + l));
+                    return;
+                }
+
+                requested.addAndGet(l);
+
+                if (inFlight) {
+                    return;
+                }
+
+                inFlight = true;
+
+                while (index < ArrayPublisher.this.array.length && requested.get() > 0) {
+
+                    if (this.cancelled) {
+                        return;
+                    }
+
+                    T item = ArrayPublisher.this.array[index];
+
+                    if (item == null) {
+                        completed = true;
+                        subscriber.onError(new NullPointerException());
+                        return;
+                    }
+
+                    index++;
+                    requested.decrementAndGet();
+                    subscriber.onNext(item);
+                }
+
+                if (index == ArrayPublisher.this.array.length) {
+                    subscriber.onComplete();
+                    completed = true;
+                }
+
+                inFlight = false;
             }
 
             @Override
             public void cancel() {
-
+                this.cancelled = true;
             }
         });
     }
--- src/test/java/reactor/ArrayPublisherTest.java
+++ src/test/java/reactor/ArrayPublisherTest.java
@@ -2,22 +2,34 @@ package reactor;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
-import sun.jvm.hotspot.utilities.Assert;
+import org.reactivestreams.tck.PublisherVerification;
+import org.reactivestreams.tck.TestEnvironment;
 
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.LongStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
-public class ArrayPublisherTest {
+public class ArrayPublisherTest extends PublisherVerification<Long> {
+
+    public static final int LATCH_TIMEOUT = 1000;
+
+    public ArrayPublisherTest() {
+        super(new TestEnvironment());
+    }
+
+    private static Long[] generate(long num) {
+        return LongStream.range(0, num >= Integer.MAX_VALUE ? 1000000 : num)
+                .boxed()
+                .toArray(Long[]::new);
+    }
 
     @Test
     public void everyMethodInSubscriberShouldBeExecutedInParticularOrder() throws InterruptedException {
@@ -47,7 +59,7 @@ public class ArrayPublisherTest {
             }
         });
 
-        Assertions.assertTrue(latch.await(1000, TimeUnit.MILLISECONDS));
+        Assertions.assertTrue(latch.await(LATCH_TIMEOUT, TimeUnit.MILLISECONDS));
         assertThat(observedSignals, contains("onSubscribe()", "onNext(0)", "onNext(1)", "onNext(2)", "onNext(3)",
                 "onNext(4)", "onComplete()"));
 
@@ -111,7 +123,7 @@ public class ArrayPublisherTest {
     @Test
     public void mustSendNPENormally() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
-        Long[] array = new Long[] {null};
+        Long[] array = new Long[]{null};
         AtomicReference<Throwable> error = new AtomicReference<>();
         ArrayPublisher<Long> publisher = new ArrayPublisher<>(array);
 
@@ -145,7 +157,6 @@ public class ArrayPublisherTest {
     @Test
     public void shouldNotDieInStackOverflow() {
         CountDownLatch latch = new CountDownLatch(1);
-        ArrayList<Long> collected = new ArrayList<>();
         long toRequest = 1;
         Long[] array = generate(toRequest);
         ArrayPublisher<Long> publisher = new ArrayPublisher<>(array);
@@ -162,7 +173,6 @@ public class ArrayPublisherTest {
 
             @Override
             public void onNext(Long aLong) {
-                collected.add(aLong);
                 s.request(1);
             }
 
@@ -176,13 +186,56 @@ public class ArrayPublisherTest {
                 latch.countDown();
             }
         });
-
     }
 
-    private static Long[] generate(long num) {
-        return LongStream.range(0, num >= Integer.MAX_VALUE ? 1000000 : num)
-                .boxed()
-                .toArray(Long[]::new);
+    @Test
+    public void shouldBePossibleToCancelSubscriptionTest() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        ArrayList<Long> collected = new ArrayList<>();
+        long toRequest = 1;
+        Long[] array = generate(toRequest);
+        ArrayPublisher<Long> publisher = new ArrayPublisher<>(array);
+
+        publisher.subscribe(new Subscriber<Long>() {
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                subscription.cancel();
+                subscription.request(10);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                collected.add(aLong);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onComplete() {
+                latch.countDown();
+            }
+        });
+
+        Assertions.assertFalse(latch.await(1000, TimeUnit.MILLISECONDS));
+        Assertions.assertEquals(0, collected.size());
     }
 
+    @Test
+    public void t() throws Throwable {
+        this.required_createPublisher3MustProduceAStreamOfExactly3Elements();
+    }
+
+
+    @Override
+    public Publisher<Long> createPublisher(long l) {
+        return new ArrayPublisher<>(generate(l));
+    }
+
+    @Override
+    public Publisher<Long> createFailedPublisher() {
+        return null;
+    }
 }
